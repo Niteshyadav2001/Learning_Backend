a03c24e32d00d3a4e021f0d2c0d549fa073b3585
Nitesh Kumar <nyrao0017@gmail.com>
2024-04-26 11:57:36 +0530

    connection-with-mongoDB-established

---
+++ src/index.js
@@ -0,0 +1,50 @@
+//2nd approach and a professional one
+import dotenv from "dotenv";
+import connectDB from "./db/index.js";
+
+dotenv.config({
+    path: '/env'
+})
+
+connectDB()
+
+
+
+
+
+
+
+
+
+
+
+
+// import mongoose from "mongoose";
+// import express from "express";
+// import { DB_NAME } from "./constants";
+
+// const app = express()
+
+
+
+//1st approach
+//sara connection ka code yahi likhenege
+
+// ( async () =>{
+//     try {
+//         await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
+
+//         app.on("error", (error)=>{
+//             console.log("ERROR", error);
+//             throw error
+//         })
+
+//         app.listen(process.env.PORT, ()=>{
+//             console.log(`App is listening on port ${process.env.PORT}`);
+//         })
+        
+//     } catch (error) {
+//         console.error("ERROR", error);
+//         throw error
+//     }
+// })
